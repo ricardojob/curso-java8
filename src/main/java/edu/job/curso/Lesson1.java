40fa4cbda7a3bb46429b7acfbfd7c8ce21056188
Ricardo Job <sousajob@gmail.com>
2015-07-19 03:48:58 -0300

    Exercicio 1 - com lambda

--- src/main/java/edu/job/curso/Lesson1.java
+++ src/main/java/edu/job/curso/Lesson1.java
@@ -10,111 +10,114 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.function.Consumer;
 
 /**
  * @author Speakjava (simon.ritter@oracle.com)
  * @author Ricardo Job
  */
 public class Lesson1 {
-  /**
-   * Run the exercises to ensure we got the right answers
-   */
-  public void runExercises() {
-    System.out.println("JDK 8 Lambdas and Streams MOOC Lesson 1");
-    System.out.println("Running exercise 1 solution...");
-    exercise1();
-    System.out.println("Running exercise 2 solution...");
-    exercise2();
-    System.out.println("Running exercise 3 solution...");
-    exercise3();
-    System.out.println("Running exercise 4 solution...");
-    exercise4();
-    System.out.println("Running exercise 5 solution...");
-    exercise5();
-  }
 
-  /**
-   * All exercises should be completed using Lambda expressions and the new
-   * methods added to JDK 8 where appropriate. There is no need to use an
-   * explicit loop in any of the code. Use method references rather than full
-   * lambda expressions wherever possible.
-   */
-  /**
-   * Exercise 1
-   *
-   * Create a string that consists of the first letter of each word in the list
-   * of Strings provided.
-   *  HINT: Use a StringBuilder to construct the result.
-   */
-  private void exercise1() {
-    List<String> list = Arrays.asList(
-        "alpha", "bravo", "charlie", "delta", "echo", "foxtrot");
-    
-    /* YOUR CODE HERE */
-  }
+    /**
+     * Run the exercises to ensure we got the right answers
+     */
+    public void runExercises() {
+        System.out.println("JDK 8 Lambdas and Streams MOOC Lesson 1");
+        System.out.println("Running exercise 1 solution...");
+        exercise1();
+        System.out.println("Running exercise 2 solution...");
+        exercise2();
+        System.out.println("Running exercise 3 solution...");
+        exercise3();
+        System.out.println("Running exercise 4 solution...");
+        exercise4();
+        System.out.println("Running exercise 5 solution...");
+        exercise5();
+    }
 
-  /**
-   * Exercise 2
-   *
-   * Remove the words that have odd lengths from the list.
-   * HINT: Use one of the new methods from JDK 8
-   */
-  private void exercise2() {
-    List<String> list = new ArrayList<>(Arrays.asList(
-        "alpha", "bravo", "charlie", "delta", "echo", "foxtrot"));
+    /**
+     * All exercises should be completed using Lambda expressions and the new
+     * methods added to JDK 8 where appropriate. There is no need to use an
+     * explicit loop in any of the code. Use method references rather than full
+     * lambda expressions wherever possible.
+     */
+    /**
+     * Exercise 1
+     *
+     * Create a string that consists of the first letter of each word in the
+     * list of Strings provided. HINT: Use a StringBuilder to construct the
+     * result.
+     */
+    private void exercise1() {
+        List<String> list = Arrays.asList(
+                "alpha", "bravo", "charlie", "delta", "echo", "foxtrot");
+        StringBuilder builder = new StringBuilder();
+        list.forEach(s -> builder.append(s.substring(0, 1)));
+        System.out.println("builder = " + builder.toString());
+    }
 
-    /* YOUR CODE HERE */
-  }
+    /**
+     * Exercise 2
+     *
+     * Remove the words that have odd lengths from the list. HINT: Use one of
+     * the new methods from JDK 8
+     */
+    private void exercise2() {
+        List<String> list = new ArrayList<>(Arrays.asList(
+                "alpha", "bravo", "charlie", "delta", "echo", "foxtrot"));
 
-  /**
-   * Exercise 3
-   *
-   * Replace every word in the list with its upper case equivalent.
-   * HINT: Use one of the new methods from JDK 8
-   */
-  private void exercise3() {
-    List<String> list = new ArrayList<>(Arrays.asList(
-        "alpha", "bravo", "charlie", "delta", "echo", "foxtrot"));
+        /* YOUR CODE HERE */
+    }
 
-    /* YOUR CODE HERE */
-  }
+    /**
+     * Exercise 3
+     *
+     * Replace every word in the list with its upper case equivalent. HINT: Use
+     * one of the new methods from JDK 8
+     */
+    private void exercise3() {
+        List<String> list = new ArrayList<>(Arrays.asList(
+                "alpha", "bravo", "charlie", "delta", "echo", "foxtrot"));
 
-  /**
-   * Exercise 4
-   *
-   * Convert every key-value pair of the map into a string and append them all
-   * into a single string, in iteration order.
-   * HINT: Again, use a StringBuilder to construct the result String. 
-   * Use one of the new JDK 8 methods for Map.
-   */
-  private void exercise4() {
-    Map<String, Integer> map = new TreeMap<>();
-    map.put("c", 3);
-    map.put("b", 2);
-    map.put("a", 1);
+        /* YOUR CODE HERE */
+    }
 
-    /* YOUR CODE HERE */
-  }
+    /**
+     * Exercise 4
+     *
+     * Convert every key-value pair of the map into a string and append them all
+     * into a single string, in iteration order. HINT: Again, use a
+     * StringBuilder to construct the result String. Use one of the new JDK 8
+     * methods for Map.
+     */
+    private void exercise4() {
+        Map<String, Integer> map = new TreeMap<>();
+        map.put("c", 3);
+        map.put("b", 2);
+        map.put("a", 1);
 
-  /**
-   * Exercise 5
-   *
-   * Create a new thread that prints the numbers from the list.
-   * HINT: This is a straightforward Lambda expression.
-   */
-  private void exercise5() {
-    List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        /* YOUR CODE HERE */
+    }
 
-    /* YOUR CODE HERE */
-  }
+    /**
+     * Exercise 5
+     *
+     * Create a new thread that prints the numbers from the list. HINT: This is
+     * a straightforward Lambda expression.
+     */
+    private void exercise5() {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-  /**
-   * Main entry point for application
-   *
-   * @param args the command line arguments
-   */
-  public static void main(String[] args) {
-    Lesson1 lesson = new Lesson1();
-    lesson.runExercises();
-  }
+        /* YOUR CODE HERE */
+    }
+
+    /**
+     * Main entry point for application
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Lesson1 lesson = new Lesson1();
+        lesson.runExercises();
+    }
 }
